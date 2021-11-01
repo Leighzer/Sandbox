6ed28c2a46bd760676af87306db7915891febfba
Leighzer <bwc_nothgiel@live.com>
2021-11-01 19:51:21 -0400

    Move factorial project;

--- cpp/Factorial/Factorial.cpp
+++ cpp/Factorial/Factorial.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
 int main()
-{   
-    // not accurate beyond 20
-    for (int i = 0; i < 20; i++) {
+{
+    for (int i = 0; i <= 20; i++) {
         unsigned long long num = (unsigned long long) i;
-        unsigned long long sum = 1l;
+        unsigned long long sum = 1ull;
         while (num > 0) {
             sum *= num--;
         }
