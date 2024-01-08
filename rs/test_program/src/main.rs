c5a83049396abff954a96ec387cf5a1c3106158e
Leighzer <bwc_nothgiel@live.com>
2024-01-07 23:42:38 -0500

    Experiment with references and lifetimes;

--- rs/test_program/src/main.rs
+++ rs/test_program/src/main.rs
@@ -5,4 +5,27 @@ fn main() {
     }
 
     println!("{}", test);
+
+    // let true_false = get_true_false(20);
+    // println!("{:?}", true_false);
+
+    // let true_false = get_true_false(35);
+    // println!("{:?}", true_false);
 }
+
+// fn get_true_false(vec_size: i32) -> Vec<&'static str> {
+//     let strings = vec!["true".to_string(), "false".to_string()];
+
+//     let mut string_references: Vec<&str> = Vec::<&str>::new();
+
+//     for i in 0..vec_size {
+//         if i % 2 == 0 {
+//             string_references.push(&strings[0]);
+//         }
+//         else {
+//             string_references.push(&strings[1]);
+//         }
+//     }
+
+//     string_references
+// }
