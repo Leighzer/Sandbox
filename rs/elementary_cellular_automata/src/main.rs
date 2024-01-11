1ab6946114f2cdb37ccf85c0a07173b30fd27262
Leighzer <bwc_nothgiel@live.com>
2024-01-11 00:24:23 -0500

    working cellular automata;

--- rs/elementary_cellular_automata/src/main.rs
+++ rs/elementary_cellular_automata/src/main.rs
@@ -1,20 +1,57 @@
-use rand::Rng;
+use std::env;
 use std::time::Duration;
+use text_colorizer::*;
+
+// TODO cleanup reduce reversing calls in this program but code does work for now
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let grid_length = 30;
+    let grid_length = 41;
+
+    // Parse the command line argument to a u8
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: {} <rule_number>", args[0]);
+        std::process::exit(1);
+    }
+    let num = args[1].parse::<u8>().expect("Error parsing ruleset arg");
+
+    let mut rule_set: Vec<bool> = u8_to_vec_bool(num);
+    rule_set.reverse();
+
+    println!("{:?}", rule_set);
 
     let mut grid: Vec<bool> = vec![];
-    for _ in 0..grid_length {
-        grid.push(rng.gen());
+    for i in 0..grid_length {
+        if i == 21 {
+            grid.push(true);
+        } else {
+            grid.push(false);
+        }
     }
     let mut new_grid: Vec<bool> = vec![];
 
     loop {
         new_grid.clear();
+
         for i in 0..grid.len() {
-            new_grid.push(!(&grid[i]) || rng.gen_range(0..100) > 90);
+            let start = match (i as i32) - 1 {
+                -1 => grid.len() - 1,
+                result => result as usize,
+            };
+            let mid = i;
+            let end = (i + 1) % grid.len();
+
+            let mut index = 0;
+            if grid[start] {
+                index = index + 1;
+            }
+            if grid[mid] {
+                index = index + 2;
+            }
+            if grid[end] {
+                index = index + 4;
+            }
+            new_grid.push(rule_set[index]);
         }
 
         println!(
@@ -22,11 +59,12 @@ fn main() {
             grid.iter()
                 .map(|&x| {
                     if x {
-                        1.to_string()
+                        1.to_string().red().bold().to_string()
                     } else {
-                        0.to_string()
+                        0.to_string().blue().to_string()
                     }
                 })
+                .rev()
                 .collect::<Vec<String>>()
                 .join(" ")
         );
@@ -34,6 +72,16 @@ fn main() {
         for i in 0..new_grid.len() {
             grid[i] = new_grid[i];
         }
-        std::thread::sleep(Duration::from_millis(50));
+        std::thread::sleep(Duration::from_millis(200));
     }
+}
+
+fn u8_to_vec_bool(num: u8) -> Vec<bool> {
+    let mut bits = vec![];
+    for i in (0..8).rev() {
+        let bit_mask = 1_u8 << i;
+        let ith_bit = num & bit_mask != 0;
+        bits.push(ith_bit);
+    }
+    bits
 }
